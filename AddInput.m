aa50b766bcce115695b49b5a84c5f9f71cc72baf
AlishKanani <aliskanani@gmail.com>
2018-10-19 17:58:48 +0530

    Update AddInput.m

--- AddInput.m
+++ AddInput.m
@@ -1,5 +1,6 @@
 function a = AddInput()
 a = zeros(4,4);
+disp('Add all entries in hexadecimal numbers as ''xxxxxxxx''');
 b(1,1:8)=input("Enter 1st Row: ");
 b(2,1:8)=input("Enter 2nd Row: ");
 b(3,1:8)=input("Enter 3rd Row: ");
